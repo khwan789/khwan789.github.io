eb817607179e3edc5a04914fd8fb7d93f8924500
Kyunghwan Sul <kyunghwan789@gmail.com>
2020-11-05 19:14:37 +0900

    game test update easier version

--- moveOver/Build/Build.framework.js
+++ moveOver/Build/Build.framework.js
@@ -1297,7 +1297,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2429728;
+STATICTOP = STATIC_BASE + 2433328;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3227,7 +3227,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2429728;
+var STATIC_BUMP = 2433328;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
@@ -15158,8 +15158,8 @@ function nullFunc_vjji(x) {
  err("Build with ASSERTIONS=2 for more info.");
  abort(x);
 }
-Module["wasmTableSize"] = 48661;
-Module["wasmMaxTableSize"] = 48661;
+Module["wasmTableSize"] = 48669;
+Module["wasmMaxTableSize"] = 48669;
 function invoke_dd(index, a1) {
  var sp = stackSave();
  try {
